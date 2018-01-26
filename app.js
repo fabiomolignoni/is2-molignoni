e27a71e134cad65486a70dde0a1a061c4f450a87
molfo96 <fabio.molignoni@studenti.unitn.it>
2018-01-26 10:58:53 +0100

    aggiunge funzione check

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
 const express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    checker = require('./checker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,12 +10,17 @@ app.set('port', (process.env.PORT || 5000));
 
 
 
+app.post('/check', function (req, res) {
+    res.status(200)
+    console.log(req.body)
+    console.log(req.body.invocationParameters)
+    return checker(req.body.url, req.body.invocationParameters, req.body.expectedResultData, req.body.expectedResultStatusz).then(function (risultato) {
+        return res.json(risultato)
+    })
 
-// a useless function that returns a fixed object. you can use it, if you want, for testing purposes
-app.get('/count',function (req, res) {
-    res.json({count: 3})
 })
 
-app.listen(app.get('port'), function() {
+
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
