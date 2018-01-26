bcd0ad747a7c899e4b27a9d9d459f01bff0f2db9
molfo96 <fabio.molignoni@studenti.unitn.it>
2018-01-26 09:43:56 +0100

    aggiunge funzione check

--- checker.js
+++ checker.js
@@ -1,8 +1,18 @@
-
 const fetch = require('node-fetch')
 
-function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
 
+function check(url, invocationParameters, expectedResultData, expectedResultStatus) {
+    url += '?'
+    var notFirst = false
+    for (var key in invocationParameters) {
+        if (!notFirst) {
+            url += '&'
+        }
+        if (invocationParameters.hasOwnProperty(key)) {
+            var val = invocationParameters[key]
+            url += (key + '=' + val)
+        }
+    }
     const checkResult = { // this is the object you need to set and return
         urlChecked: url,
         resultData: null,
@@ -10,9 +20,19 @@ function check(url, invocationParameters,  expectedResultData, expectedResultSta
         statusTestPassed: null,
         resultDataAsExpected: null
     }
-
-
-
+    console.log(url)
+    return fetch(url).then(function (response) {
+        checkResult.resultStatus = response.status
+        checkResult.statusTestPassed = (response.status === expectedResultStatus)
+        return response.json()
+    }).then(function (json) {
+        checkResult.resultData = json
+        checkResult.resultDataAsExpected = compareResults(json, expectedResultData)
+        return checkResult
+    }).catch(function (error) {
+        console.log('Unexpected error', error)
+        return checkResult // ritorno di default checkResult, i campi che non sono stati calcolati saranno comunque null
+    })
 }
 
 
@@ -22,7 +42,7 @@ function compareResults(expected, actual) {
     if (!expected) return true //always ok if there are no expectations
     if (!actual) return false
     for (let e of Object.keys(expected)) {
-        if (actual[e]===undefined || expected[e]!=actual[e]  ) return false
+        if (actual[e] === undefined || expected[e] != actual[e]) return false
     }
     return true
 }
